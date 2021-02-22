9d2488c0385bd2c2cff356e0aa02e69f68669853
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2021-02-22 10:06:32 +0300

    [OPS-1150] Make helvetios and enif use different backup repositories
    When they use the same repository and run backup job at the same time,
    one of the servers may fail to acquire the lock on the repo

--- servers/helvetios/backups.nix
+++ servers/helvetios/backups.nix
@@ -6,7 +6,7 @@
 
   services.borgbackup.jobs.backup = {
     # https://www.notion.so/serokell/Rsync-net-797d5fdca3744aed8e17db741b7fce5a
-    repo = "12482@ch-s012.rsync.net:www";
+    repo = "12482@ch-s012.rsync.net:www-staging";
     paths = [ "/var/lib/www/files" ];
   };
 }
