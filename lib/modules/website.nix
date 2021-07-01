bb5f98277981604e90d971c6ba95311e7a812312
Alexander Bantyev <balsoft@balsoft.ru>
2021-07-01 21:01:19 +0300

    Turn blog admins into a list for readability

--- lib/modules/website.nix
+++ lib/modules/website.nix
@@ -130,8 +130,19 @@ in {
     environment = {
       # PGUSER and PGDATABASE set at server level
       PGHOST = "matar";
-      ADMINS =
-        "artem.shelest@serokell.io aw@serokell.io denis.oleynikov@serokell.io dmitrii.susloparov@serokell.io gints.dreimanis@serokell.io jm@serokell.io kirelagin@serokell.io leonid.cupikov@serokell.io olga.bolgurtseva@serokell.io seroka@serokell.io yulia.gavrilova@serokell.io";
+      ADMINS = toString [
+        "artem.shelest@serokell.io"
+        "aw@serokell.io"
+        "denis.oleynikov@serokell.io"
+        "dmitrii.susloparov@serokell.io"
+        "gints.dreimanis@serokell.io"
+        "jm@serokell.io"
+        "kirelagin@serokell.io"
+        "leonid.cupikov@serokell.io"
+        "olga.bolgurtseva@serokell.io"
+        "seroka@serokell.io"
+        "yulia.gavrilova@serokell.io"
+      ];
     };
 
     serviceConfig = {
